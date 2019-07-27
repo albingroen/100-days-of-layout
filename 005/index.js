fd75fa1ac1f40f0f44202937cf389efe6608ebb9
Albin Groen <albin.groen@gmail.com>
2019-07-28 01:45:02 +0200

    Added profile images

--- 005/index.js
+++ 005/index.js
@@ -13,6 +13,13 @@ import {
   faSignOutAlt,
   faEllipsisV
 } from "@fortawesome/free-solid-svg-icons";
+import profile_0 from "./assets/profiles/profile_0.jpg";
+import profile_3 from "./assets/profiles/profile_3.jpg";
+import profile_5 from "./assets/profiles/profile_5.jpg";
+import profile_8 from "./assets/profiles/profile_8.jpg";
+import profile_9 from "./assets/profiles/profile_9.jpg";
+import profile_10 from "./assets/profiles/profile_10.jpg";
+import profile_11 from "./assets/profiles/profile_11.jpg";
 
 const sideBarItems = [
   {
@@ -140,8 +147,13 @@ const Card = ({ course }) => (
     <hr />
     <footer>
       <div className="author">
-        <div className="profile" />
-        <span>{course.author}</span>
+        <div
+          className="profile"
+          style={{
+            backgroundImage: `url(${course.author.img})`
+          }}
+        />
+        <span>{course.author.name}</span>
       </div>
       <button>Continue</button>
     </footer>
@@ -155,7 +167,10 @@ const courses = [
     category: "Frontend development",
     description:
       "Master Angular 5 from the basics to building an advanced application with Firebase´s Firestore as well...",
-    author: "Brad Traversy"
+    author: {
+      name: "Brad Traversy",
+      img: profile_0
+    }
   },
   {
     title: "Codeigniter",
@@ -163,7 +178,9 @@ const courses = [
     category: "Backend development",
     description:
       "Learn Php Codeigniter and understand working with MVC and HMVC from zero to hero",
-    author: "InsideCode M"
+    author: {
+      name: "InsideCode M"
+    }
   },
   {
     title: "Laravel",
@@ -171,7 +188,9 @@ const courses = [
     category: "Backend development",
     description:
       "Build a RESTful API for a market system using Laravel and dominates the challenging RESTful skills",
-    author: "JuanD MeGon"
+    author: {
+      name: "JuanD MeGon"
+    }
   },
   {
     title: "NodeJS",
@@ -179,7 +198,10 @@ const courses = [
     category: "Backend development",
     description:
       "Dive deep under the hood of NodeJS. Learn V8, Express, the MEAN stack, core Javascript concepts, and more.",
-    author: "Anthony Alicea"
+    author: {
+      name: "Anthony Alicea",
+      img: profile_3
+    }
   },
   {
     title: "AngularJS",
@@ -187,7 +209,10 @@ const courses = [
     category: "Frontend development",
     description:
       "Master Angular 5 from the basics to building an advanced application with Firebase´s Firestore as well...",
-    author: "Brad Traversy"
+    author: {
+      name: "Brad Traversy",
+      img: profile_0
+    }
   },
   {
     title: "Sketch",
@@ -195,7 +220,10 @@ const courses = [
     category: "UI / UX Design",
     description:
       "Finally a comprehensive guide to using Sketch for designing mobile. Learn to design an app from A to Z.",
-    author: "Joseph Angelo"
+    author: {
+      name: "Joseph Angelo",
+      img: profile_5
+    }
   },
   {
     title: "Bootstrap",
@@ -203,7 +231,9 @@ const courses = [
     category: "Web Design",
     description:
       "This tutorial has been prepared for anyone who has a basic knowledge of HTML and CSS to develop websites.",
-    author: "Janice Carroll"
+    author: {
+      name: "Janice Carroll"
+    }
   },
   {
     title: "Firebase",
@@ -211,7 +241,9 @@ const courses = [
     category: "Backend development",
     description:
       "Full-stack Development with Angular 8, Firestore, Firebase Storage&Hosting. Firebase Cloud Functions..",
-    author: "Sara Perkins"
+    author: {
+      name: "Wayne Patel"
+    }
   },
   {
     title: "Github",
@@ -219,7 +251,10 @@ const courses = [
     category: "Version Control",
     description:
       "Go from complete novice to expert in Git and GitHub using step-by-step, no-assumotions learning.",
-    author: "Sara Perkins"
+    author: {
+      name: "Sara Perkins",
+      img: profile_8
+    }
   },
   {
     title: "Principle",
@@ -227,7 +262,10 @@ const courses = [
     category: "Interaction design",
     description:
       "Learn to prototype UI animations and micro-interactions using Sketch, Flinto, Principle, Marvel & Invision Craft Plugin",
-    author: "Joshua Burton"
+    author: {
+      name: "Joshua Burton",
+      img: profile_9
+    }
   },
   {
     title: "ReactJS",
@@ -235,7 +273,10 @@ const courses = [
     category: "Frontend Development",
     description:
       "Dive in and learn React 16.8 from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js...",
-    author: "Debra Oliver"
+    author: {
+      name: "Debra Oliver",
+      img: profile_10
+    }
   },
   {
     title: "YII Framework",
@@ -243,11 +284,14 @@ const courses = [
     category: "Backend Framework",
     description:
       "Fast track your web application development by harnessing the power of the YII PHP framework.",
-    author: "Deborah Pena"
+    author: {
+      name: "Deborah Pena",
+      img: profile_11
+    }
   }
 ];
 
-const Dashboard = () => (
+const Dashboard = ({ images }) => (
   <div className="dashboard">
     <h2>
       My courses for <span>"All Courses"</span>
@@ -269,7 +313,7 @@ export default class App extends Component {
     return (
       <div className="wrapper">
         <Sidebar />
-        <Dashboard />
+        <Dashboard images={this.state.images} />
       </div>
     );
   }
